2bf7c6ae00945a99a149019d66ff116e8b1af7a4
sys-nyx <nyx@pwnvault.com>
2024-12-31 17:08:19 -0800

    build test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://pwnvault.com',
-  tagline: 'Own the web',
+  tagline: 'Own the webs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
